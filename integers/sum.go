9f41cf7542935ab7959488ee19281a2ccfdccaff
Mateo Galic <mateogalic112@gmail.com>
2026-09-23 12:51:27 +0200

    add sumAllTails functions

--- integers/sum.go
+++ integers/sum.go
@@ -16,3 +16,18 @@ func SumAll(slicesOfNumbers ...[]int) []int {
 
 	return sums
 }
+
+func SumAllTails(slicesOfNumbers ...[]int) []int {
+	var sums []int
+	for _, numbers := range slicesOfNumbers {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
+}
