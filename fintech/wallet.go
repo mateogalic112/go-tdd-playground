f69dcf79b2552b780d21a5df59a85e83437cd25b
Mateo Galic <mateogalic112@gmail.com>
2026-09-24 14:35:25 +0200

    add fintech domain

---
+++ fintech/wallet.go
@@ -0,0 +1,17 @@
+// Package fintech handles wallet interactions
+package fintech
+
+import "fmt"
+
+type Wallet struct {
+	balance int
+}
+
+func (w *Wallet) Deposit(amount int) {
+	fmt.Printf("address of balance in Deposit is %p \n", &w.balance)
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() int {
+	return w.balance
+}
