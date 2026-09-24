8a06e8030bdb37dac5ff30fe28d0b3b6806bb67a
Mateo Galic <mateogalic112@gmail.com>
2026-09-24 13:07:39 +0200

    add shape interface

---
+++ geometry/shapes.go
@@ -0,0 +1,21 @@
+// Package geometry for working with shapes
+package geometry
+
+import "math"
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
